984d188c48d27ad4d16e33e72fbbda9c23682f94
st10769 <st10769@iismarconipieralisi.it>
2026-04-20 07:05:56 +0000

    Inizio implementazione rotte Partecipa

--- src/api/php/routes.php
+++ src/api/php/routes.php
@@ -8,18 +8,31 @@ $routes = [
         // Rotta custom che usa un suo handler specifico
         '/api/prova' => 'mostra_messaggio_di_prova',
         // Rotte che usano il gestore generico per tabelle
+        '/api/users' => 'generic_table_handler',
+        '/api/products' => 'generic_table_handler',
+        '/api/orders' => 'get_orders_join',
+        '/api/partecipa' => 'get_all_partecipa',     // lista tutto
+
         '/api/branche' => 'get_branche'
     ],
 
     // Rotte che rispondono al metodo POST
     'POST' => [
+        '/api/products' => 'create_product',
+        '/api/auth' => 'authenticate_user',
+        'api/partecipa' =>  'create_partecipa',
         '/api/auth' => 'authenticate_user'
     ],
     
     'PUT' => [
+        '/api/products/:id' => 'update_product',
+        '/api/partecipa/:id_attivita/:id_unita' => 'update_partecipa',
         //'/api/products/:id' => 'update_product'
        // '/api/products/:id' => 'update_product'
     ],
     
+    'DELETE' => [
+        '/api/partecipa/:id_attivita/:id_unita' => 'delete_partecipa',
+    ]
     // Puoi aggiungere qui altri metodi come PUT, DELETE, etc.
 ];
